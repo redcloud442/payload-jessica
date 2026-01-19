9308e30252f80197b8de9c8c559573f8b1256c40
redcloud442 <cloudred442@gmail.com>
2026-01-19 14:28:22 +0800

    fix media url

--- src/components/landing-page/portfolio/portfolioview.tsx
+++ src/components/landing-page/portfolio/portfolioview.tsx
@@ -67,12 +67,7 @@ export default function PortfolioView({ projectsByCategory }: any) {
                         >
                           {isVideo ? (
                             <video
-                              src={
-                                process.env.NODE_ENV === "development"
-                                  ? media.url
-                                  : process.env.NEXT_PUBLIC_MEDIA_URL +
-                                    media.url
-                              }
+                              src={media.url}
                               autoPlay
                               muted
                               playsInline
